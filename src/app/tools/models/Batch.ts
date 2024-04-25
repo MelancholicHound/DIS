294ab4178f246dd68e068b0e5d8615cf62c483f4
Soliloquist <furyx.resonance@gmail.com>
2024-04-25 21:23:08 +0800

    modal ready

---
+++ src/app/tools/models/Batch.ts
@@ -0,0 +1,8 @@
+export interface Batch {
+  id : number;
+  batchId : string;
+  supplier : string;
+  dateDelivered : string; //change to Date
+  dateTested : string; //change to Date
+  validUntil : string; //change to Date
+}
