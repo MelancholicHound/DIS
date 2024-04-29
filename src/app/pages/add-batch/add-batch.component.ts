7e804f5ed4d9e399b0d50fb620b2f4d6b5281f4a
Soliloquist <furyx.resonance@gmail.com>
2024-04-29 15:23:16 +0800

    add-batch continuation

--- src/app/pages/add-batch/add-batch.component.ts
+++ src/app/pages/add-batch/add-batch.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
 import { AuthService } from '../../tools/services/auth.service';
 
 @Component({
   selector: 'app-add-batch',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule
+  ],
   templateUrl: './add-batch.component.html',
   styleUrl: './add-batch.component.css'
 })
@@ -15,6 +18,13 @@ export class AddBatchComponent implements OnInit {
   constructor(private router : Router, private authService : AuthService) {}
 
   tempBatch: any;
+
+  devices: any[] = [
+    {id: 1, device: 'COMPUTER', division: 'HOPSS', conns: 3 }
+  ];
+
+  dataSource = [...this.devices];
+  displayedColumns: string [] = ['Device', 'Division', 'Section', 'No. of Connections'];
 
   ngOnInit(): void {
      const fetchedBatch = this.authService.getTempBatch();
