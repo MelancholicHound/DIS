e3e9c4867a73448f829a87cff7fe1a7b0638cfd4
Soliloquist <furyx.resonance@gmail.com>
2024-05-06 16:18:09 +0800

    aio configuration

--- src/app/pages/devices/aio/aio.component.ts
+++ src/app/pages/devices/aio/aio.component.ts
@@ -16,5 +16,12 @@ import { SoftwaresComponent } from '../../../components/softwares/softwares.comp
   styleUrl: './aio.component.css'
 })
 export class AioComponent {
-
+  toggler() {
+    const toggle = document.getElementById('toggler-peripheral') as HTMLInputElement;
+    if (toggle.checked) {
+      console.log('toggle');
+    } else {
+      console.log("not toggled");
+    }
+  }
 }
